96197891790405e62a2c75b29e6f4bf93403dc55
Drq13112 <davidredquin@gmail.com>
2025-05-26 12:13:11 +0200

    ignore

--- build/tensorrt_yolo/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/david/yolocpp_ws/src/TensorRT-YOLO
-# Build directory: /home/david/yolocpp_ws/build/tensorrt_yolo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tensorrt_yolo__py")
--- build/yolocpp/CMakeFiles/yolocpp_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/yolocpp_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/yolocpp_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
